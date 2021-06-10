ae6de8f7a2a511cb6eeea83f11c2c837747ede49
Pavel Shherbina <pks@parametrica.team>
2021-06-10 22:38:02 +0300

    Construct/deconstruct cell components, goo changes
    There are some points with displaying cell values. Only requested from spreadsheet cells got formatted value
    so manually created cell have only user entered value.
    
    Also UserEnteredValue and CellFormat need own Goo for displaying its own output and CellData output in nearest future.
    For now CellData Goo uses UserEnteredValues directly.

---
+++ BookWorm/Construct/GhConstructCell.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using BookWorm.Goo;
+using Google.Apis.Sheets.v4.Data;
+using Grasshopper.Kernel;
+
+namespace BookWorm.Construct
+{
+    /// <summary>
+    /// Construct GoogleSheets CellData from some of it's component parts.
+    /// </summary>
+    public class GhConstructCell : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GhConstructCell class.
+        /// </summary>
+        public GhConstructCell()
+          : base(
+                "Construct Cell",
+                "ConCell",
+                "Construct cell from some of it's component parts",
+                "BookWorm",
+                "Construct")
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter(
+                "User Entered Value",
+                "UV",
+                "The value the user entered in the cell.",
+                GH_ParamAccess.item);
+
+            pManager.AddGenericParameter("Cell Format", "CF", "Cell Format", GH_ParamAccess.item);
+        }
+
+        /// <inheritdoc/>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Cell", "C", "Cell", GH_ParamAccess.item);
+        }
+
+        /// <inheritdoc/>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            ExtendedValue inputUserEnteredValue = null;
+            CellFormat inputUserCellFormat = null;
+
+            if (!DA.GetData(0, ref inputUserEnteredValue))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Null in UV input");
+            }
+
+            if (!DA.GetData(1, ref inputUserCellFormat))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Null in CF input");
+            }
+
+            var cellData = new CellData
+            {
+                UserEnteredValue = inputUserEnteredValue,
+                UserEnteredFormat = inputUserCellFormat,
+            };
+
+            var cellDataGoo = new GH_CellData(cellData);
+
+            DA.SetData(0, cellDataGoo);
+        }
+
+        /// <inheritdoc/>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("bd29230b-9399-4248-aae6-5ea70b922770"); }
+        }
+    }
+}
---
+++ BookWorm/Deconstruct/GhDeconstructCell.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using BookWorm.Goo;
+using Grasshopper.Kernel;
+
+namespace BookWorm.Deconstruct
+{
+    /// <summary>
+    /// Deconstruct GoogleSheets CellData by some of it's component parts.
+    /// </summary>
+    public class GhDeconstructCell : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the GhDeconstructCell class.
+        /// </summary>
+        public GhDeconstructCell()
+          : base(
+                "Deconstruct Cell",
+                "DeCell",
+                "Deconstruct cell by some of it's component parts",
+                "BookWorm",
+                "Deconstruct")
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Cell", "C", "Cell", GH_ParamAccess.item);
+        }
+
+        /// <inheritdoc/>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Formatted Value", "FV", "Formatted value of the cell", GH_ParamAccess.item);
+
+            pManager.AddGenericParameter(
+                "User Entered Value",
+                "UV",
+                "The value the user entered in the cell. e.g, 1234 , 'Hello' , or =NOW()."
+                + "\n Note: Dates, Times and DateTimes are represented as doubles in serial number format.",
+                GH_ParamAccess.item);
+
+            pManager.AddGenericParameter("Cell Format", "CF", "Cell Format", GH_ParamAccess.item);
+        }
+
+        /// <inheritdoc/>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            GH_CellData cellDataGoo = null;
+
+            if (!DA.GetData(0, ref cellDataGoo) || !cellDataGoo.IsValid)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Null in Cell input");
+                return;
+            }
+
+            var cellData = cellDataGoo.Value;
+
+            var formattedValue = cellData.FormattedValue;
+            var userEnteredValue = cellData.UserEnteredValue;
+            var cellFormat = cellData.UserEnteredFormat;
+
+            DA.SetData(0, formattedValue);
+            DA.SetData(1, userEnteredValue);
+            DA.SetData(2, cellFormat);
+        }
+
+        /// <inheritdoc/>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <inheritdoc/>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("a5f68522-f13d-42c9-a855-4b1d01a63da8"); }
+        }
+    }
+}
