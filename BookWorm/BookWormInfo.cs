1f9153bf16cc690afe2b8baa7cab7beadc9dda91
Max <max@malein.com>
2021-07-15 13:44:22 +0300

    0.7.0 - a lot of new features and components

--- BookWorm/BookWormInfo.cs
+++ BookWorm/BookWormInfo.cs
@@ -1,11 +1,15 @@
-﻿using Grasshopper.Kernel;
-using System;
+﻿using System;
 using System.Drawing;
+using Grasshopper.Kernel;
 
 namespace GoogleDocs
 {
+    /// <summary>
+    /// Info.
+    /// </summary>
     public class BookWormInfo : GH_AssemblyInfo
     {
+        /// <inheritdoc/>
         public override string Name
         {
             get
@@ -14,18 +18,23 @@ namespace GoogleDocs
             }
         }
 
+        /// <inheritdoc/>
         public override Bitmap Icon
         {
             get
             {
-                //Return a 24x24 pixel bitmap to represent this GHA library.
+                // Return a 24x24 pixel bitmap to represent this GHA library.
                 return null;
             }
         }
 
+        /// <inheritdoc/>
         public override string Description => "GoogleDocs connector. The description is really long and thorough";
-        public override string Version => "0.6.2";
 
+        /// <inheritdoc/>
+        public override string Version => "0.7.0";
+
+        /// <inheritdoc/>
         public override Guid Id
         {
             get
@@ -34,20 +43,22 @@ namespace GoogleDocs
             }
         }
 
+        /// <inheritdoc/>
         public override string AuthorName
         {
             get
             {
-                //Return a string identifying you or your company.
+                // Return a string identifying you or your company.
                 return "Max Malein";
             }
         }
 
+        /// <inheritdoc/>
         public override string AuthorContact
         {
             get
             {
-                //Return a string representing your preferred contact details.
+                // Return a string representing your preferred contact details.
                 return @"https://github.com/max-malein/BookWorm";
             }
         }
