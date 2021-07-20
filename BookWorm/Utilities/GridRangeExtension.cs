e743e491970bc16742319c7fd89f3a80cd73b914
elena21march <zev@parametrica.team>
2021-07-20 20:20:04 +0300

    grid range from A1 & new fit range to cells

--- BookWorm/Utilities/GridRangeExtension.cs
+++ BookWorm/Utilities/GridRangeExtension.cs
@@ -13,18 +13,42 @@ namespace BookWorm.Utilities
         /// <returns>Fitted range.</returns>
         public static GridRange FitRangeToCells(this GridRange gridRange, int cellsCount)
         {
+
             // Only columns case.
             // Works fine without that because row length depends on columns indicies (for GetRows),
             // so you don't actually need end row index. But explicit is better than implicit.
-            if (gridRange.EndRowIndex == null)
+
+            // Case A5
+            if (gridRange.EndColumnIndex == null && gridRange.EndRowIndex == null && cellsCount == 1)
+            {
+                gridRange.EndColumnIndex = gridRange.StartColumnIndex + 1;
+                gridRange.EndRowIndex = gridRange.StartRowIndex + 1;
+            }
+
+            // Case A:B
+            if (gridRange.StartRowIndex == null && gridRange.EndRowIndex == null)
             {
                 var colCount = gridRange.EndColumnIndex - gridRange.StartColumnIndex;
                 gridRange.StartRowIndex = 0;
                 gridRange.EndRowIndex = (int)Math.Ceiling((double)cellsCount / (int)colCount);
             }
 
-            // Only rows case.
-            else if (gridRange.EndColumnIndex == null)
+            // Case A5:A
+            if (gridRange.EndRowIndex == null)
+            {
+                var colCount = gridRange.EndColumnIndex - gridRange.StartColumnIndex;
+                gridRange.EndRowIndex = gridRange.StartRowIndex + (int)Math.Ceiling((double)cellsCount / (int)colCount);
+            }
+
+            // Case B5:5
+            if (gridRange.EndColumnIndex == null && gridRange.StartColumnIndex != null)
+            {
+                var colCount = gridRange.EndColumnIndex - gridRange.StartColumnIndex;
+                gridRange.EndColumnIndex = gridRange.StartColumnIndex + (int)Math.Ceiling((double)cellsCount / (int)colCount);
+            }
+
+            // Case 2:4
+            else if (gridRange.StartColumnIndex == null && gridRange.EndColumnIndex == null)
             {
                 var rowCount = gridRange.EndRowIndex - gridRange.StartRowIndex;
                 gridRange.StartColumnIndex = 0;
